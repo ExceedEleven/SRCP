b4123a6c10477d7af867adde13906c68c5518df6
MinZung <90918826+minza55113151@users.noreply.github.com>
2023-02-09 22:21:06 +0700

    endpoint declare

--- Backend/router/parking.py
+++ Backend/router/parking.py
@@ -8,3 +8,39 @@ from fastapi import APIRouter, Body, HTTPException
 router = APIRouter(prefix="/park",
                    tags=["park"])
 
+
+@router.get("/", status_code=200)
+def get_park():
+    pass
+
+
+@router.get("/{park_id}", status_code=200)
+def get_park(park_id: int):
+    pass
+
+
+@router.put("/update/{park_id}", status_code=200)
+def update_park(park_id: int):
+    pass
+
+
+@router.get("/barrier/{park_id}", status_code=200)
+def get_barrier(park_id: int):
+    pass
+
+
+@router.put("/barrier/{park_id}", status_code=200)
+def update_barrier(park_id: int):
+    pass
+
+
+@router.post("/reserved/{park_id}/{user_id}", status_code=200)
+def reserved_park(park_id: int, user_id: int):
+    pass
+
+
+@router.delete("/reserved/{park_id}", status_code=200)
+def delete_reserved_park(park_id: int):
+    pass
+
+
