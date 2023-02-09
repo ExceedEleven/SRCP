b7583d88c2ac4f7a1849026c87b7b967a5b3117b
MinZung <90918826+minza55113151@users.noreply.github.com>
2023-02-09 22:34:13 +0700

    update model

--- Backend/router/model.py
+++ Backend/router/model.py
@@ -2,23 +2,29 @@ from pydantic import BaseModel
 from datetime import datetime
 
 class User(BaseModel):
-    user_id: int
+    user_id: str
+    park_id: str
     username: str
     password: str
-    credits_card: int
-    park_id: int
+    credits_card: str
     jwt: str
     
     
 class CarPark(BaseModel):
-    park_id: int
+    park_id: str
     state: str
     is_open: bool
     time_start: datetime
     
 
 class Payment(BaseModel):
-    payment_id: int
-    user_id: int
+    payment_id: str
+    user_id: str
     fee: int
     time_payment: datetime
+    
+    
+class Reservation(BaseModel):
+    user_id: str
+    park_id: str
+    time_expire: datetime
