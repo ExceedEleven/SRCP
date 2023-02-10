94c9f063c1e6f4c2273b1887403b7a9a4cd9a854
MinZung <90918826+minza55113151@users.noreply.github.com>
2023-02-10 13:10:05 +0700

    user comment

--- Backend/router/user.py
+++ Backend/router/user.py
@@ -8,17 +8,20 @@ router = APIRouter(prefix="/user",
 def get_user():
     pass
 
-
+# optional
 @router.put("/update/{user_id}", status_code=200)
 def update_user():
     pass
 
-
+# username
+# password
 @router.post("/signin", status_code=200)
 def signin():
     pass
 
-
+# username 
+# password
+# credit card
 @router.post("/signup", status_code=200)
 def signup():
     pass
