8d432b6280b277770a34d1da8e3bfd11e0ba4cb8
koakatta <koakatta@gmail.com>
2023-02-10 11:00:16 +0700

    add return fee in SetBarrier and lint

--- frontend/src/services/postdata.js
+++ frontend/src/services/postdata.js
@@ -1,13 +1,15 @@
-import axios from "axios"
+import axios from 'axios';
 
 export async function SetBarrier(id) {
-  const res = await axios.put(`http://group11.exceed19.online/park/barrier/${id}`)
-  console.log(res)
+  const res = await axios.put(`http://group11.exceed19.online/park/barrier/${id}`);
+  console.log(res.message);
+  return res.fee;
 }
 export async function SetReserve(pid, uid) {
-  const res = await axios.post(`http://group11.exceed19.online/park/reserved/${pid}/${uid}`)
-  console.log(res)
+  const res = await axios.post(`http://group11.exceed19.online/park/reserved/${pid}/${uid}`);
+  console.log(res);
 }
-export async function Delete(pid)
-const res = await axios.post(`http://group11.exceed19.online/park/reserved/${pid}`)
-console.log(res)
+export async function Delete(pid) {
+  const res = await axios.post(`http://group11.exceed19.online/park/reserved/${pid}`);
+  console.log(res);
+}
